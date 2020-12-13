e6ad322f1be01ed33949f140d3c9b30811a10d12
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-14 05:42:56 +0700

    First Router for articles

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-import express from 'express';
+// import express from 'express';
+const express = require('express');
+const articleRouter = require('./routes/articles.js');
 
 const app = express();
 const PORT = 5000;
 
 app.listen(PORT, () =>console.log(`Server Running on port: http://localhost:${PORT}`));
+
+app.use('/articles', articleRouter)
 
 app.set('view engine', 'ejs')
 
