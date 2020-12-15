2283c9737cc5fafbebfb037f32dd9e048ca4179f
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-15 09:04:39 +0700

    Hooked up to database

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // import express from 'express';
 const express = require('express');
 const articleRouter = require('./routes/articles.js');
-
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 5000;
+
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
 
 app.listen(PORT, () =>console.log(`Server Running on port: http://localhost:${PORT}`));
 
