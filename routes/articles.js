e8e9b28c97280afe7948caac6048854e8696deea
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-15 08:55:12 +0700

    Make new article route and layout

--- routes/articles.js
+++ routes/articles.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/new", (req, res) => res.render('article/new'));
-// router.get("/", (req, res) => res.send('in articles'));
+router.get("/new", (req, res) => res.render('articles/new'));
+router.post("/", (req, res) => res.send('post new article'));
 
 module.exports = router
